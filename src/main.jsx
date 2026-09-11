ca8eee94f28a7c3cd8273cac3059119ed339171c
barbarasouzza <barbarasoouzza@gmail.com>
2026-09-11 13:17:21 -0300

    fix: corrige caminho do GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/festival-gabiroba_site">
+    <BrowserRouter basename="/festival_gabiroba_site">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/festival-gabiroba_site/',
+  base: '/festival_gabiroba_site/',
 })
